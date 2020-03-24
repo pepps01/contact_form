ccc390370b2c5ad54fd4012245681d5761e7b943
pepps01 <slpepple01@gmail.com>
2020-03-24 19:30:56 +0100

    Initial Commit

---
+++ processForm.php
@@ -0,0 +1,13 @@
+<?php
+    if(!empty($_POST)){
+        echo "<pre>";
+            print_r($_POST);
+        echo "</pre>";
+
+        // Test the file actually works 
+
+        // store data in a csv file
+    }else{
+        echo "No data was transferred";
+    }
+?>
